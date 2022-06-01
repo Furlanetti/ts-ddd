68ede2a7e0b8dc2aa4cd3372e83ce0dec87d5648
Furlanetti <felipe.furlanetti1@gmail.com>
2022-05-31 21:40:07 -0300

    Add extra test to customer to check add reward points

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -42,4 +42,13 @@ describe("Customer unit test", () => {
 
     expect(customer.isActive()).toBe(false);
   });
+
+  it("should add reward points", () => {
+    const customer = new Customer("1234", "John");
+    expect(customer.rewardPoints).toBe(0)
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(10)
+    customer.addRewardPoints(100);
+    expect(customer.rewardPoints).toBe(110)
+  });
 });
